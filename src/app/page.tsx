a49b8b93dc46fd711cfd647e97ab8bce0849cf2d
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-12-17 12:21:17 +0530

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+const mockURLs = [
+  "https://utfs.io/f/Oym9FJnXJ3flmYqJuKUR7C4tOWfbD6Nu5sVklgMaXFUcJpnZ",
+  "https://utfs.io/f/Oym9FJnXJ3flmYqJuKUR7C4tOWfbD6Nu5sVklgMaXFUcJpnZ",
+  "https://utfs.io/f/Oym9FJnXJ3flL3szGduXsbcTmlVKthHg7rUiwnuN2ojApxzJ",
+];
+
+const mockImages = mockURLs.map((url, index) => ({
+  id: index,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Snapbase
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((img) => (
+          <div key={img.id} className="w-48">
+            <img src={img.url} alt="images" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
