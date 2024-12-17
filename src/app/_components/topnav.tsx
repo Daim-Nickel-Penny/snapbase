ae4cecf78c6aaf20d79c8aa720777c1b0841480d
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-12-17 14:21:57 +0530

    Auth Added

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Snapbase</div>
+
+      <div className="flex flex-row items-center gap-4">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
