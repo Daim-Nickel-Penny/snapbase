a844757b14e2c786063483b083af201de180df1e
Daim-Nickel-Penny <1804449@kiit.ac.in>
2024-12-17 17:21:12 +0530

    Get Image info in modal

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,15 +1,30 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-
+  const client_local_clerk = await clerkClient();
+  const userInfo = await client_local_clerk.users.getUser(image.userId);
   return (
     <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
       <div className="flex-shrink flex-grow">
+        {/*es-lint-ignore-next*/}
         <img src={image.url} className="object-contain" alt={image.name} />
       </div>
       <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
-        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+        <div className="border-b p-2 text-center text-xl">
+          {image.name.substring(0, 20) + "..."}
+        </div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div>{userInfo.fullName}</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
       </div>
     </div>
   );
